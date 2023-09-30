605e8c4ff538fa3e6eb2ddaad628e326e694db03
Artem Puzik <artenni4@gmail.com>
2023-09-30 17:03:22 +0200

    added dependency injection

--- Supermarket.Wpf/App.xaml.cs
+++ Supermarket.Wpf/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Supermarket.Core.Employees;
+using Supermarket.Infrastructure.Employees;
+using Supermarket.Wpf.Login;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +17,38 @@ namespace Supermarket.Wpf
     /// </summary>
     public partial class App : Application
     {
+        private readonly IServiceProvider _serviceProvider;
+
+        public App()
+        {
+            var serviceCollection = new ServiceCollection();
+            AddCore(serviceCollection);
+            AddInfrastructure(serviceCollection);
+            AddWpf(serviceCollection);
+
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var loginWindow = _serviceProvider.GetRequiredService<LoginWindow>();
+            loginWindow.Show();
+        }
+
+        private void AddCore(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IEmployeeService, EmployeeService>();
+        }
+
+        private void AddInfrastructure(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IEmployeeRepository, EmployeeRepository>();
+        }
+
+        private void AddWpf(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<LoginWindow>();
+            serviceCollection.AddSingleton<LoginViewModel>();
+        }
     }
 }
