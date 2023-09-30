6fae9f0ff19d8b24cd9b0b021ad874b1e2f3e83e
Artem Puzik <artenni4@gmail.com>
2023-09-30 17:19:43 +0200

    repository base, IEntity

---
+++ Supermarket.Infrastructure/Common/CrudRepositoryBase.cs
@@ -0,0 +1,29 @@
+﻿using Supermarket.Core.Common;
+
+namespace Supermarket.Infrastructure.Common
+{
+    internal abstract class CrudRepositoryBase<TEntity, TId> where TEntity : IEntity<TId>
+    {
+        // TODO add basic CRUD operations for entities
+
+        protected Task<TEntity> GetByIdAsync(TId id)
+        {
+            return null!;
+        }
+
+        protected Task<TId> AddAsync(TEntity entity)
+        {
+            return null!;
+        }
+
+        protected Task UpdateAsync(TEntity entity)
+        {
+            return null!;
+        }
+
+        protected Task DeleteAsync(TId id)
+        {
+            return null!;
+        }
+    }
+}
