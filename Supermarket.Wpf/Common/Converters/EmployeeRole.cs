315ea41ecb2eb3e3c17e03c285e67787970d02ab
Artem Puzik <artenni4@gmail.com>
2023-11-06 22:25:08 +0100

    default styles

--- Supermarket.Wpf/Common/Converters/EmployeeRole.cs
+++ Supermarket.Wpf/Common/Converters/EmployeeRole.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Supermarket.Wpf.Common
+namespace Supermarket.Wpf.Common.Converters
 {
     internal enum EmployeeRole
     {
