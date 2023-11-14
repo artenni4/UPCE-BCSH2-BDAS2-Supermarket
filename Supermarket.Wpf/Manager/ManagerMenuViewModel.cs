51604f9d2f46ba016f3b47d17bc73adc7c498d99
Artem Puzik <artenni4@gmail.com>
2023-11-14 12:22:11 +0100

    cleanup

--- Supermarket.Wpf/Manager/ManagerMenuViewModel.cs
+++ Supermarket.Wpf/Manager/ManagerMenuViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.ObjectModel;
-using System.Windows;
 using Supermarket.Wpf.Manager.AddProducts;
 using Supermarket.Wpf.Manager.SupermarketCashboxes;
 using Supermarket.Wpf.Manager.SupermarketEmployees;
@@ -8,14 +7,13 @@ using Supermarket.Wpf.Manager.SupermarketProducts;
 using Supermarket.Wpf.Manager.SupermarketSales;
 using Supermarket.Wpf.Manager.SupermarketStorages;
 using Supermarket.Wpf.ViewModelResolvers;
-using System.Windows.Controls;
-using System.Windows.Input;
 
 namespace Supermarket.Wpf.Manager
 {
     public class ManagerMenuViewModel : NotifyPropertyChangedBase, IViewModel, IAsyncInitialized
     {
         private readonly IViewModelResolver _viewModelResolver;
+        public ObservableCollection<ITabViewModel> TabViewModels { get; } = new();
         
         private ITabViewModel? _selectedTabViewModel;
         public ITabViewModel? SelectedTabViewModel
@@ -30,57 +28,6 @@ namespace Supermarket.Wpf.Manager
                 }
             }
         }
-
-        private SupermarketProductsViewModel? _supermarketProductsViewModel;
-        public SupermarketProductsViewModel? SupermarketProductsViewModel
-        {
-            get => _supermarketProductsViewModel;
-            set => SetProperty(ref _supermarketProductsViewModel, value);
-        }
-
-        private AddProductsViewModel? _addProductsViewModel;
-        public AddProductsViewModel? AddProductsViewModel
-        {
-            get => _addProductsViewModel;
-            set => SetProperty(ref _addProductsViewModel, value);
-        }
-
-        private SupermarketEmployeesViewModel? _supermarketEmployeesViewModel;
-        public SupermarketEmployeesViewModel? SupermarketEmployeesViewModel
-        {
-            get => _supermarketEmployeesViewModel;
-            set => SetProperty(ref _supermarketEmployeesViewModel, value);
-        }
-
-        private SupermarketStoragesViewModel? _supermarketStoragesViewModel;
-        public SupermarketStoragesViewModel? SupermarketStoragesViewModel
-        {
-            get => _supermarketStoragesViewModel;
-            set => SetProperty(ref _supermarketStoragesViewModel, value);
-        }
-
-        private SupermarketLogsViewModel? _supermarketLogsViewModel;
-        public SupermarketLogsViewModel? SupermarketLogsViewModel
-        {
-            get => _supermarketLogsViewModel;
-            set => SetProperty(ref _supermarketLogsViewModel, value);
-        }
-
-        private SupermarketSalesViewModel? _supermarketSalesViewModel;
-        public SupermarketSalesViewModel? SupermarketSalesViewModel
-        {
-            get => _supermarketSalesViewModel;
-            set => SetProperty(ref _supermarketSalesViewModel, value);
-        }
-
-        private SupermarketCashboxesViewModel? _supermarketCashboxesViewModel;
-        public SupermarketCashboxesViewModel? SupermarketCashboxesViewModel
-        {
-            get => _supermarketCashboxesViewModel;
-            set => SetProperty(ref _supermarketCashboxesViewModel, value);
-        }
-
-        public ObservableCollection<ITabViewModel> TabViewModels { get; } = new();
         
         public ManagerMenuViewModel(IViewModelResolver viewModelResolver)
         {
@@ -89,22 +36,23 @@ namespace Supermarket.Wpf.Manager
 
         public async Task InitializeAsync()
         {
-            SupermarketProductsViewModel = await _viewModelResolver.Resolve<SupermarketProductsViewModel>();
-            AddProductsViewModel = await _viewModelResolver.Resolve<AddProductsViewModel>();
-            SupermarketEmployeesViewModel = await _viewModelResolver.Resolve<SupermarketEmployeesViewModel>();
-            SupermarketStoragesViewModel = await _viewModelResolver.Resolve<SupermarketStoragesViewModel>();
-            SupermarketLogsViewModel = await _viewModelResolver.Resolve<SupermarketLogsViewModel>();
-            SupermarketSalesViewModel = await _viewModelResolver.Resolve<SupermarketSalesViewModel>();
-            SupermarketCashboxesViewModel = await _viewModelResolver.Resolve<SupermarketCashboxesViewModel>();
+            var supermarketProductsViewModel = await _viewModelResolver.Resolve<SupermarketProductsViewModel>();
+            var addProductsViewModel = await _viewModelResolver.Resolve<AddProductsViewModel>();
+            var supermarketEmployeesViewModel = await _viewModelResolver.Resolve<SupermarketEmployeesViewModel>();
+            var supermarketStoragesViewModel = await _viewModelResolver.Resolve<SupermarketStoragesViewModel>();
+            var supermarketLogsViewModel = await _viewModelResolver.Resolve<SupermarketLogsViewModel>();
+            var supermarketSalesViewModel = await _viewModelResolver.Resolve<SupermarketSalesViewModel>();
+            var supermarketCashboxesViewModel = await _viewModelResolver.Resolve<SupermarketCashboxesViewModel>();
             
-            TabViewModels.Add(SupermarketProductsViewModel);
-            TabViewModels.Add(AddProductsViewModel);
-            TabViewModels.Add(SupermarketEmployeesViewModel);
-            TabViewModels.Add(SupermarketLogsViewModel);
-            TabViewModels.Add(SupermarketSalesViewModel);
-            TabViewModels.Add(SupermarketCashboxesViewModel);
+            TabViewModels.Add(supermarketProductsViewModel);
+            TabViewModels.Add(addProductsViewModel);
+            TabViewModels.Add(supermarketEmployeesViewModel);
+            TabViewModels.Add(supermarketStoragesViewModel);
+            TabViewModels.Add(supermarketLogsViewModel);
+            TabViewModels.Add(supermarketSalesViewModel);
+            TabViewModels.Add(supermarketCashboxesViewModel);
             
-            SelectedTabViewModel = SupermarketProductsViewModel;
+            SelectedTabViewModel = supermarketProductsViewModel;
         }
     }
 }
