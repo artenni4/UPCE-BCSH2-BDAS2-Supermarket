ec82830c162ff58852ee6531203721862b444a0b
Artem Puzik <artenni4@gmail.com>
2023-11-18 21:40:05 +0100

    add transaction for add sale

--- Supermarket.Infrastructure/Common/UnitOfWork.cs
+++ Supermarket.Infrastructure/Common/UnitOfWork.cs
@@ -14,6 +14,7 @@ namespace Supermarket.Infrastructure.Common
 
         public Task<ITransaction> BeginTransactionAsync()
         {
+            _dbConnection.Open();
             var dbTransaction = _dbConnection.BeginTransaction();
             return Task.FromResult(new Transaction(dbTransaction) as ITransaction);
         }
