53af1326a734570881a244de4223f2436c5e449d
serhii.dudchenko <maillennur@gmail.com>
2023-11-18 23:44:39 +0100

    triggers fix

--- Scripts/triggers.sql
+++ Scripts/triggers.sql
@@ -9,7 +9,18 @@ END;
 CREATE OR REPLACE TRIGGER trg_set_null_supermarket_id
 BEFORE INSERT OR UPDATE ON ZAMESTNANCI
 FOR EACH ROW
+DECLARE
+    v_role_id NUMBER;
 BEGIN
-    :NEW.supermarket_id := NULL;
+    BEGIN
+        SELECT role_id INTO v_role_id FROM ROLE_ZAMESTNANCU WHERE zamestnanec_id = :NEW.zamestnanec_id AND role_id = 1;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            -- Обработка случая, когда не найдено ни одной записи
+            v_role_id := NULL;
+    END;
+
+    IF v_role_id IS NOT NULL THEN
+        :NEW.supermarket_id := NULL;
+    END IF;
 END;
-/
