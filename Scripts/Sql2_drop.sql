7021e1153b780264c4e81a9574c72457981cc2dc
serhii.dudchenko <maillennur@gmail.com>
2023-11-22 12:57:16 +0100

    sqls

---
+++ Scripts/Sql2_drop.sql
@@ -0,0 +1,48 @@
+DROP TABLE dodavatele CASCADE CONSTRAINTS;
+
+DROP TABLE druhy_zbozi CASCADE CONSTRAINTS;
+
+DROP TABLE logy CASCADE CONSTRAINTS;
+
+DROP TABLE merne_jednotky CASCADE CONSTRAINTS;
+
+DROP TABLE mista_ulozeni CASCADE CONSTRAINTS;
+
+DROP TABLE platba CASCADE CONSTRAINTS;
+
+DROP TABLE pokladny CASCADE CONSTRAINTS;
+
+DROP TABLE prodane_zbozi CASCADE CONSTRAINTS;
+
+DROP TABLE prodavane_zbozi CASCADE CONSTRAINTS;
+
+DROP TABLE prodeje CASCADE CONSTRAINTS;
+
+DROP TABLE regiony CASCADE CONSTRAINTS;
+
+DROP TABLE role CASCADE CONSTRAINTS;
+
+DROP TABLE role_zamestnancu CASCADE CONSTRAINTS;
+
+DROP TABLE soubory CASCADE CONSTRAINTS;
+
+DROP TABLE supermarkety CASCADE CONSTRAINTS;
+
+DROP TABLE typy_placeni CASCADE CONSTRAINTS;
+
+DROP TABLE ulozeni_zbozi CASCADE CONSTRAINTS;
+
+DROP TABLE zamestnanci CASCADE CONSTRAINTS;
+
+DROP TABLE zbozi CASCADE CONSTRAINTS;
+
+DROP SEQUENCE dodavatele_dodavatel_id_seq;
+DROP SEQUENCE druhy_zbozi_druh_zbozi_id_seq;
+DROP SEQUENCE mista_ulozeni_misto_ulozeni_id;
+DROP SEQUENCE pokladny_pokladna_id_seq;
+DROP SEQUENCE prodeje_prodej_id_seq;
+DROP SEQUENCE regiony_region_id_seq;
+DROP SEQUENCE supermarkety_supermarket_id;
+DROP SEQUENCE zamestnanci_zamestnanec_id_seq;
+DROP SEQUENCE zbozi_zbozi_id_seq;
+DROP SEQUENCE soubory_soubor_id_seq;
